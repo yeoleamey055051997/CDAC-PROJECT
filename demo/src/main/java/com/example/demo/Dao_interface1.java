cd8662175ae09b7bab1ad616ec29f3288b044e1d
Amey <72598957+yeoleamey055051997@users.noreply.github.com>
2021-03-16 10:18:48 +0530

    16/03/2021 of project

--- demo/src/main/java/com/example/demo/Dao_interface1.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface Dao_interface1 extends JpaRepository<Parking_details, Integer>{
-
-}
